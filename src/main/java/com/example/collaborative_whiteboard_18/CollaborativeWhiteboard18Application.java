74c493c42d064950ddb895d988cb3e84e0e2682b
ajnr2456 <arjunnagar486@gmail.com>
2026-05-05 13:43:44 +0530

    Initial commit of project template

---
+++ src/main/java/com/example/collaborative_whiteboard_18/CollaborativeWhiteboard18Application.java
@@ -0,0 +1,13 @@
+package com.example.collaborative_whiteboard_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeWhiteboard18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborativeWhiteboard18Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/collaborative_whiteboard_18/CollaborativeWhiteboard18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collaborative_whiteboard_18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeWhiteboard18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
